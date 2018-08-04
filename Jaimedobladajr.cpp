9ae84a72e771bd05418d0e570690edf42d37ca68
doblada0119 <40530870+doblada0119@users.noreply.github.com>
2018-08-04 22:52:20 +0800

    Add files via upload

---
+++ Jaimedobladajr.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+int main(){
+	cout<<"-----------------Chicken adobo recipe-----------------\n\n";
+
+    int Chicken=3;
+
+    int Soysauce=1;
+
+    int Vinegar=2;
+
+    int GarlicClove=1;
+
+    int Bayleaves=2;
+
+    int Peppercorn=1;
+
+    
+
+    
+
+
+
+    cout<<endl<<"Chicken = "<<Chicken <<" lbs\n";
+
+    cout<<endl<<"Soy sauce = "<<Soysauce<<"/2 cup\n";
+
+    cout<<endl<<"Vinegar = "<<Vinegar<<"/3 cup\n";
+
+    cout<<endl<<"Galic clove = "<<GarlicClove<<" pcs crushed\n ";
+
+    cout<<endl<<"Bay leaves = "<<Bayleaves<<" pcs\n";
+
+    cout<<endl<<"Peppercorn = "<<Peppercorn<<" teaspoon\n";
+
+    cout<<endl<<"Salt = Depends on your taste\n\n";
+
+
+
+    cout<<endl<<"Directions: \n\n";
+
+
+
+    cout<<endl<<"1. Bring to a boil the first 6 ingredients.\n"
+    
+	"2. Simmer for about 50 minutes until the chicken is tender and sauce is reduced by half.\n"
+	
+	"3. Let simmer with the lid on for about 35 minutes and cook with the lid off for the last 15 minutes to let the sauce reduce.\n------------------------------------------------------";
+
+    return 0;
+
+}
